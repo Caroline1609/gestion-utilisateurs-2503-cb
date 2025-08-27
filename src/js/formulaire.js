01ea569e345b78186a0fed876ec4a2c597787b7f
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-08-27 15:50:26 +0200

    upload

--- src/js/formulaire.js
+++ src/js/formulaire.js
@@ -1,4 +1,12 @@
 const firstname = document.getElementById('firstname');
 const email = document.getElementById('email');
 const role = document.getElementById('role');
+const add = document.getElementById('add')
 
+form.addEventListener('submit', addUser)
+
+
+
+function addUser(){
+
+}
