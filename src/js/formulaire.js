f8591438deaac7840210a2f51c3f891b84e673f5
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-08-27 16:55:54 +0200

    upload

--- src/js/formulaire.js
+++ src/js/formulaire.js
@@ -1,12 +1,45 @@
 const firstname = document.getElementById('firstname');
 const email = document.getElementById('email');
 const role = document.getElementById('role');
-const add = document.getElementById('add')
+const add = document.getElementById('add');
+const formUser = document.getElementById('formUser');
+const resultat = document.getElementById('resultat');
 
-form.addEventListener('submit', addUser)
+formUser.addEventListener('submit', addUser);
 
+function addUser(event) {
+    event.preventDefault();
 
+    const isFirstnameValid = checkFirstname(firstname.value);
+    const isEmailValid = checkEmail(email.value);
 
-function addUser(){
+    if (isFirstnameValid || isEmailValid ) {
+        console.log("valider");
+    } else {
+        console.log("erreur");
+        console.log(email.value)
+    }
+}
+
+/**
+ * Valide un prénom
+ * 3 caractères minimum, uniquement des lettres (et tirets au besoin)
+ * @param {string} _firstname Le prénom à valider
+ * @returns {Boolean} VRAI si valide, FAUX sinon
+ */
+function checkFirstname(_firstname) {
+
+    let regexFirstname = new RegExp('^[A-Za-zÀ-ÖØ-öø-ÿ]{3,}([-][A-Za-zÀ-ÖØ-öø-ÿ]+)*$');
+    _firstname = _firstname.trim();
+
+    return regexFirstname.test(_firstname);
+}
+
+function checkEmail(_email){
+    let regexEmail = /^[\w\.-]+@[\w]+\.[a-z]+$/;
+    _email = _email.trim();
+
+    return regexEmail.test(_email);
 
 }
+
